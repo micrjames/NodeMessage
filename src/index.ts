725903cee97e340606241b8b5df491af19f61af6
Michael James <micrjames921@gmail.com>
2025-02-14 23:59:18 -0700

    moved 'handleRequest' code to function; added 'error' handling code

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,18 @@ import http, { IncomingMessage, ServerResponse } from 'http';
 
 const PORT = process.env.PORT || 3000;
 
-http.createServer((_: IncomingMessage, res: ServerResponse) => {
-   res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.write('Hello, World!');
-   res.end();
-}).listen(PORT, () => {
+const handleRequest = (_: IncomingMessage, res: ServerResponse): void => {
+   try {
+	  res.writeHead(200, {'Content-Type': 'text/plain'});
+	  res.write('Hello, World!');
+	  res.end();
+   } catch(error) {
+	  console.error('Error handling request:', error);
+	  res.writeHead(500, {'Content-Type': 'text/plain'});
+	  res.end();
+   }
+};
+
+http.createServer(handleRequest).listen(PORT, () => {
    console.log(`Running on port ${PORT}`);
 });
