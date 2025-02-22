2f0d142830379e8724d0f99e38c1179633f84e95
Michael James <micrjames921@gmail.com>
2025-02-22 08:25:06 -0700

    added 'error handling' to 'index'

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,14 @@ import Server from "./libs/Server.js";
 const PORT = process.env.PORT || 3000;
 
 const server = new Server(typeof PORT === 'string' ? parseInt(PORT, 10) : PORT);
-// server.start();
-server.start().catch(console.error);
+
+(() => {
+   try {
+	  // server.start();
+	  server.start().catch(console.error);
+	  console.log('Server started successfully.');
+   } catch(error) {
+	  console.error('Failed to start server:', error);
+	  process.exit(1);
+   }
+})();
